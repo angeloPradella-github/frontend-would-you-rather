aa0e655d05d23f1b9afdbe0d0f39f8d18d466ee3
angeloPradella-github <angelogpradella@gmail.com>
2023-09-14 12:12:36 +0200

    hero section completatat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'; // Importa l'icona del pulsante di riproduzione
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,5 @@ import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'; // Importa l'i
 export class HomeComponent {
   siteName: string = environment.siteName;
   playIcon = faPlayCircle;
+  arrowRight = faArrowRight;
 }
