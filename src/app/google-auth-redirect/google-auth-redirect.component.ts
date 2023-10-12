8dbc7636c9768cfc6579b466e2c4cc379ac6ac8d
angeloPradella-github <angelogpradella@gmail.com>
2023-10-12 16:02:05 +0200

    login con google fatto

--- src/app/google-auth-redirect/google-auth-redirect.component.ts
+++ src/app/google-auth-redirect/google-auth-redirect.component.ts
@@ -11,25 +11,17 @@ export class GoogleAuthRedirectComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      console.log("Parametri ricevuti nell'URL:", params);
+      if (params) {
+        const data = JSON.parse(params['data']);
 
-      const data = JSON.parse(params['data']);
-      console.log("Dati JSON ricevuti nell'URL:", data);
+        // Esegui l'elaborazione dei dati e salvali nel localStorage
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('userData', JSON.stringify(data.user));
 
-      // Esegui l'elaborazione dei dati e salvali nel localStorage
-      localStorage.setItem('token', data.token);
-      localStorage.setItem('userData', JSON.stringify(data.user));
-
-      console.log(
-        'Dati salvati nel localStorage:',
-        localStorage.getItem('token'),
-        localStorage.getItem('userData')
-      );
-
-      // Esegui eventuali altre azioni di logout come reindirizzamento
-      this.router.navigate(['/']);
-      // Ricarica la pagina
-      // location.reload();
+        this.router.navigate(['/']);
+      } else {
+        this.router.navigate(['/login']);
+      }
     });
   }
 }
