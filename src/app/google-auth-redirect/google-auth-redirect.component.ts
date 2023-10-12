b771c577fb17b74b1d2457b9882c96cfbafad6c0
angeloPradella-github <angelogpradella@gmail.com>
2023-10-12 11:34:19 +0200

    creato componente di reindiraazomento google

--- src/app/google-auth-redirect/google-auth-redirect.component.ts
+++ src/app/google-auth-redirect/google-auth-redirect.component.ts
@@ -11,12 +11,10 @@ export class GoogleAuthRedirectComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      console.log(
-        "Dati ricevuti nell'endpoint di reindirizzamento:",
-        params['data']
-      );
+      console.log("Parametri ricevuti nell'URL:", params);
 
       const data = JSON.parse(params['data']);
+      console.log("Dati JSON ricevuti nell'URL:", data);
 
       // Esegui l'elaborazione dei dati e salvali nel localStorage
       localStorage.setItem('token', data.token);
